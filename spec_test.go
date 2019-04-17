531c5e50db7bc03e211b15014db3ae25b7a90c84
Kolokotronis Panagiotis <panos122008@gmail.com>
2019-04-17 18:37:52 +0300

    Add further tests

--- spec_test.go
+++ spec_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/panagiks/SPEC/storage"
 )
 
+// Adapted to the needs of this project from:
+// https://golang.org/src/testing/example.go
 func captureOutput(f func()) (got string) {
 	stdout := os.Stdout
 	r, w, _ := os.Pipe()
@@ -31,6 +33,17 @@ func captureOutput(f func()) (got string) {
 	return
 }
 
+func TestExecutorAdd(t *testing.T) {
+	// Test missing arguments
+	expected := "Must select what to add; Options {project, task}!"
+	output := captureOutput(func() {
+		executorAdd([]string{})
+	})
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+}
+
 func TestExecutorAddProject(t *testing.T) {
 	// Test missing arguments
 	expected := "Usage: \"add project {name}\"!"
@@ -48,6 +61,44 @@ func TestExecutorAddProject(t *testing.T) {
 	})
 	found := false
 	for k := range dataStore {
+		if k == expected {
+			found = true
+		}
+	}
+	if output != "" {
+		t.Errorf("\nExpexted: no output\nGot: \t\t'%s'\n", output)
+	}
+	if !found {
+		t.Errorf("\nExpexted: \t'%s' project not found\n", expected)
+	}
+}
+
+func TestExecutorAddTask(t *testing.T) {
+	// Test missing arguments
+	expected := "Usage: \"add task {project} {name} {estimation} {confidence}\"!"
+	dataStore = storage.NewStorage()
+	output := captureOutput(func() {
+		executorAddTask([]string{})
+	})
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+	// Test invalid project name
+	output = captureOutput(func() {
+		executorAddTask([]string{"ProjectName", "TaskName", "1.0", "0.5"})
+	})
+	expected = "No project with name: \"ProjectName\"! Please create one with \"add project\" command!"
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+	// Test task addition
+	dataStore["ProjectName"] = storage.NewProject()
+	expected = "TaskName"
+	output = captureOutput(func() {
+		executorAddTask([]string{"ProjectName", "TaskName", "1.0", "0.5"})
+	})
+	found := false
+	for k := range dataStore["ProjectName"] {
 		if k == expected {
 			found = true
 		}
