fe8368bad892aeb738202ef111ee46668a585e7e
Kolokotronis Panagiotis <panos122008@gmail.com>
2019-04-18 22:30:31 +0300

    Redesign cli as a state machine

--- spec.go
+++ spec.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime"
 	"strconv"
 	"strings"
 
@@ -9,109 +11,214 @@ import (
 	"github.com/panagiks/SPEC/storage"
 )
 
+type cliState struct {
+	executor           func(in []string)
+	completer          func(t prompt.Document) []prompt.Suggest
+	LivePrefix         string
+	IsLivePrefixEnable bool
+	project            string
+	task               string
+}
+
 var (
 	dataStore storage.SPECStorage
+	cli       []cliState
 )
 
-func executorAdd(blocks []string) {
-	if len(blocks) < 1 {
-		fmt.Println("Must select what to add; Options {project, task}!")
-		return
-	}
-	switch blocks[0] {
-	case "task":
-		executorAddTask(blocks[1:])
-	case "project":
-		executorAddProject(blocks[1:])
+func updateTask(project string, name string, actual string) {
+	if actualF, err := strconv.ParseFloat(actual, 64); err == nil {
+		_, ok := dataStore[project]
+		if !ok {
+			fmt.Printf("No project with name: \"%s\"! Please create one!\n", project)
+			return
+		}
+		_, ok = dataStore[project][name]
+		if !ok {
+			fmt.Printf("No task with name: \"%s\"! Please create one with \"add\" command!\n", name)
+			return
+		}
+		dataStore[project][name].Actual = actualF
 	}
 }
 
-func executorAddTask(blocks []string) {
-	if len(blocks) < 4 {
-		fmt.Println("Usage: \"add task {project} {name} {estimation} {confidence}\"!")
-		return
-	}
-	if median, err := strconv.ParseFloat(blocks[2], 64); err == nil {
-		if sigma, err := strconv.ParseFloat(blocks[3], 64); err == nil {
-			_, ok := dataStore[blocks[0]]
+func addTask(project string, name string, median string, sigma string) {
+	if medianF, err := strconv.ParseFloat(median, 64); err == nil {
+		if sigmaF, err := strconv.ParseFloat(sigma, 64); err == nil {
+			_, ok := dataStore[project]
 			if !ok {
-				fmt.Printf("No project with name: \"%s\"! Please create one with \"add project\" command!\n", blocks[0])
+				fmt.Printf("No project with name: \"%s\"! Please create one with \"add\" command!\n", project)
 				return
 			}
-			dataStore[blocks[0]][blocks[1]] = storage.NewTask(blocks[1], median, sigma)
+			dataStore[project][name] = storage.NewTask(name, medianF, sigmaF)
 		}
 	}
 }
 
-func executorAddProject(blocks []string) {
-	if len(blocks) < 1 {
-		fmt.Println("Usage: \"add project {name}\"!")
-		return
-	}
-	dataStore[blocks[0]] = storage.NewProject()
-}
-
-func executorList(blocks []string) {
-	if len(blocks) < 1 {
-		fmt.Println("Must select what to list; Options {project, task}!")
-		return
-	}
-	switch blocks[0] {
-	case "project":
-		executorListProject()
-	case "task":
-		executorListTask(blocks[1:])
-	}
-}
-
-func executorListTask(blocks []string) {
-	if len(blocks) < 1 {
-		fmt.Println("When listing tasks you must define a project!")
-		return
-	}
-	_, ok := dataStore[blocks[0]]
+func listTask(project string) {
+	_, ok := dataStore[project]
 	if !ok {
-		fmt.Printf("No project with name: \"%s\"! Please create one with \"add project\" command!\n", blocks[0])
+		fmt.Printf("No project with name: \"%s\"! Please create one!\n", project)
 		return
 	}
-	storage.PrintTasks(dataStore[blocks[0]])
+	storage.PrintTasks(dataStore[project])
 }
 
-func executorListProject() {
-	storage.PrintProjects(&dataStore)
+func addProject(name string) {
+	dataStore[name] = storage.NewProject()
+}
+
+func listProject() {
+	dataStore.PrintProjects()
+}
+
+func selectTask(project string, name string) {
+	_, ok := dataStore[project][name]
+	if !ok {
+		fmt.Printf("No task with name: \"%s\"! Please create one with \"add\" command!\n", name)
+		return
+	}
+	cli = append(
+		cli,
+		cliState{
+			executor:           taskExecutor,
+			completer:          taskCompleter,
+			IsLivePrefixEnable: true,
+			LivePrefix:         fmt.Sprintf("[P:%s][T:%s] >", project, name),
+			project:            project,
+			task:               name,
+		},
+	)
+}
+
+func selectProject(project string) {
+	_, ok := dataStore[project]
+	if !ok {
+		fmt.Printf("No project with name: \"%s\"! Please create one with \"add\" command!\n", project)
+		return
+	}
+	cli = append(
+		cli,
+		cliState{
+			executor:           projectExecutor,
+			completer:          projectCompleter,
+			IsLivePrefixEnable: true,
+			LivePrefix:         fmt.Sprintf("[P:%s] >", project),
+			project:            project,
+		},
+	)
+}
+
+func taskExecutor(blocks []string) {
+	switch blocks[0] {
+	case "actual":
+		if len(blocks) < 2 {
+			fmt.Println("Usage: \"actual {duration}\"!")
+			return
+		}
+		updateTask(cli[len(cli)-1].project, cli[len(cli)-1].task, blocks[1])
+	}
+}
+
+func taskCompleter(t prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "actual", Description: "Set the selected task's actual duration; Syntax: \"actual {duration}\""},
+	}
+	return s
+}
+
+func projectExecutor(blocks []string) {
+	switch blocks[0] {
+	case "add", "a":
+		if len(blocks) < 4 {
+			fmt.Println("Usage: \"add {name} {estimation} {confidence}\"!")
+			return
+		}
+		addTask(cli[len(cli)-1].project, blocks[1], blocks[2], blocks[3])
+	case "list", "l":
+		listTask(cli[len(cli)-1].project)
+	default:
+		selectTask(cli[len(cli)-1].project, blocks[0])
+	}
+}
+
+func projectCompleter(t prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "add", Description: "Add a new task; Syntax: \"add {taskName} {estimation} {confidence}\""},
+		{Text: "list", Description: "List tasks within the project"},
+	}
+	for k := range dataStore[cli[len(cli)-1].project] {
+		s = append(s, prompt.Suggest{Text: k, Description: "Select task for further options"})
+	}
+	return s
+}
+
+func baseExecutor(blocks []string) {
+	switch blocks[0] {
+	case "add", "a":
+		if len(blocks) < 2 {
+			fmt.Println("Usage: \"add {projectName}\"!")
+			return
+		}
+		addProject(blocks[1])
+	case "list", "l":
+		listProject()
+	default:
+		selectProject(blocks[0])
+	}
+}
+
+func baseCompleter(t prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "add", Description: "Add a new project; Syntax: \"add {projectName}\""},
+		{Text: "list", Description: "List projects"},
+	}
+	for k := range dataStore {
+		s = append(s, prompt.Suggest{Text: k, Description: "Select project for further options"})
+	}
+	return s
+}
+
+func goBack() {
+	if len(cli) == 1 {
+		runtime.Goexit()
+	}
+	cli = cli[:len(cli)-1]
 }
 
 func executor(in string) {
 	in = strings.TrimSpace(in)
-	blocks := strings.Split(in, " ")
-	switch blocks[0] {
-	case "add":
-		executorAdd(blocks[1:])
-	case "list":
-		executorList(blocks[1:])
+	if in == "" {
+		goBack()
+		return
 	}
+	blocks := strings.Split(in, " ")
+	cli[len(cli)-1].executor(blocks)
 }
 
 func completer(t prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "add"},
-		{Text: "add project"},
-		{Text: "add task"},
-		{Text: "list"},
-		{Text: "list task"},
-		{Text: "list project"},
-	}
+	s := cli[len(cli)-1].completer(t)
 	return prompt.FilterHasPrefix(s, t.GetWordBeforeCursor(), true)
+}
+
+func changeLivePrefix() (string, bool) {
+	return cli[len(cli)-1].LivePrefix, cli[len(cli)-1].IsLivePrefixEnable
 }
 
 func main() {
 	dataStore = storage.NewStorage()
+	cli = append(
+		cli,
+		cliState{executor: baseExecutor, completer: baseCompleter},
+	)
 	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program.")
+	defer os.Exit(0)
 	defer fmt.Println("Bye!")
 	p := prompt.New(
 		executor,
 		completer,
 		prompt.OptionTitle("SPEC: Software Project Estimation Calculator"),
+		prompt.OptionPrefix("> "),
+		prompt.OptionLivePrefix(changeLivePrefix),
 	)
 	p.Run()
 }
--- spec_test.go
+++ spec_test.go
@@ -33,31 +33,12 @@ func captureOutput(f func()) (got string) {
 	return
 }
 
-func TestExecutorAdd(t *testing.T) {
-	// Test missing arguments
-	expected := "Must select what to add; Options {project, task}!"
-	output := captureOutput(func() {
-		executorAdd([]string{})
-	})
-	if output != expected {
-		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
-	}
-}
-
-func TestExecutorAddProject(t *testing.T) {
-	// Test missing arguments
-	expected := "Usage: \"add project {name}\"!"
-	dataStore = storage.NewStorage()
-	output := captureOutput(func() {
-		executorAddProject([]string{})
-	})
-	if output != expected {
-		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
-	}
+func TestAddProject(t *testing.T) {
 	// Test successful project creation
-	expected = "ProjectName"
-	output = captureOutput(func() {
-		executorAddProject([]string{"ProjectName"})
+	dataStore = storage.NewStorage()
+	expected := "ProjectName"
+	output := captureOutput(func() {
+		addProject("ProjectName")
 	})
 	found := false
 	for k := range dataStore {
@@ -73,21 +54,43 @@ func TestExecutorAddProject(t *testing.T) {
 	}
 }
 
-func TestExecutorAddTask(t *testing.T) {
-	// Test missing arguments
-	expected := "Usage: \"add task {project} {name} {estimation} {confidence}\"!"
+func TestSelectProject(t *testing.T) {
 	dataStore = storage.NewStorage()
+	// Test invalid project name
 	output := captureOutput(func() {
-		executorAddTask([]string{})
+		selectProject("ProjectName")
 	})
+	expected := "No project with name: \"ProjectName\"! Please create one with \"add\" command!"
 	if output != expected {
 		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
 	}
-	// Test invalid project name
+	expected = "ProjectName"
+	dataStore["ProjectName"] = storage.NewProject()
+	// dataStore["ProjectName"]["TaskName"] = storage.NewTask("TaskName", 2, 0.5)
 	output = captureOutput(func() {
-		executorAddTask([]string{"ProjectName", "TaskName", "1.0", "0.5"})
+		selectProject("ProjectName")
 	})
-	expected = "No project with name: \"ProjectName\"! Please create one with \"add project\" command!"
+	found := false
+	for k := range cli {
+		if cli[k].project == expected {
+			found = true
+		}
+	}
+	if output != "" {
+		t.Errorf("\nExpexted: no output\nGot: \t\t'%s'\n", output)
+	}
+	if !found {
+		t.Errorf("\nExpexted: \t'%s' task not found\n", expected)
+	}
+}
+
+func TestAddTask(t *testing.T) {
+	dataStore = storage.NewStorage()
+	// Test invalid project name
+	output := captureOutput(func() {
+		addTask("ProjectName", "TaskName", "1.0", "0.5")
+	})
+	expected := "No project with name: \"ProjectName\"! Please create one with \"add\" command!"
 	if output != expected {
 		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
 	}
@@ -95,7 +98,7 @@ func TestExecutorAddTask(t *testing.T) {
 	dataStore["ProjectName"] = storage.NewProject()
 	expected = "TaskName"
 	output = captureOutput(func() {
-		executorAddTask([]string{"ProjectName", "TaskName", "1.0", "0.5"})
+		addTask("ProjectName", "TaskName", "1.0", "0.5")
 	})
 	found := false
 	for k := range dataStore["ProjectName"] {
@@ -108,5 +111,68 @@ func TestExecutorAddTask(t *testing.T) {
 	}
 	if !found {
 		t.Errorf("\nExpexted: \t'%s' project not found\n", expected)
+	}
+}
+
+func TestUpdateTask(t *testing.T) {
+	dataStore = storage.NewStorage()
+	// Test invalid project name
+	output := captureOutput(func() {
+		updateTask("ProjectName", "TaskName", "1.0")
+	})
+	expected := "No project with name: \"ProjectName\"! Please create one!"
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+	// Test invalid task name
+	dataStore["ProjectName"] = storage.NewProject()
+	output = captureOutput(func() {
+		updateTask("ProjectName", "TaskName", "1.0")
+	})
+	expected = "No task with name: \"TaskName\"! Please create one with \"add\" command!"
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+}
+
+func TestListTask(t *testing.T) {
+	dataStore = storage.NewStorage()
+	// Test invalid project name
+	output := captureOutput(func() {
+		listTask("ProjectName")
+	})
+	expected := "No project with name: \"ProjectName\"! Please create one!"
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+}
+
+func TestSelectTask(t *testing.T) {
+	dataStore = storage.NewStorage()
+	// Test invalid project name
+	output := captureOutput(func() {
+		selectTask("ProjectName", "TaskName")
+	})
+	expected := "No task with name: \"TaskName\"! Please create one with \"add\" command!"
+	if output != expected {
+		t.Errorf("\nExpexted: \t'%s'\nGot: \t\t'%s'\n", expected, output)
+	}
+	expected = "TaskName"
+	dataStore["ProjectName"] = storage.NewProject()
+	dataStore["ProjectName"]["TaskName"] = storage.NewTask("TaskName", 2, 0.5)
+	output = captureOutput(func() {
+		selectTask("ProjectName", "TaskName")
+	})
+	found := false
+	for k := range cli {
+		if cli[k].task == expected {
+			found = true
+		}
+	}
+	if output != "" {
+		t.Errorf("\nExpexted: no output\nGot: \t\t'%s'\n", output)
+	}
+	if !found {
+		t.Errorf("\nExpexted: \t'%s' task not found\n", expected)
 	}
 }
